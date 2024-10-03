e63d696d843ae8a52e69906c4b165799e156b5d2
enble <enble_777@naver.com>
2024-10-04 00:08:30 +0900

    add EchoGUI

---
+++ src/project/EchoClientGUI.java
@@ -0,0 +1,213 @@
+package project;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class EchoClientGUI {
+
+    private final JFrame frame;
+    private final String serverAddress;
+    private final int serverPort;
+
+    private JTextArea t_display;
+    private JButton sendButton;
+
+    private Writer out;
+    private Reader in;
+
+    public EchoClientGUI(String serverAddress, int serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;   
+
+        frame = new JFrame("EchoClient GUI");
+
+        buildGUI();
+
+        frame.setSize(400, 300);
+        frame.setLocation(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    /*
+     * GUI related methods
+     */
+    private void buildGUI() {
+        frame.add(createDisplayPanel(), BorderLayout.CENTER);
+
+        JPanel panel = new JPanel(new GridLayout(2, 0));
+        panel.add(createInputPanel());
+        panel.add(createControlPanel());
+
+        frame.add(panel, BorderLayout.SOUTH);
+    }
+
+    private JPanel createDisplayPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        JScrollPane scrollPane = new JScrollPane();
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        t_display = new JTextArea();
+        t_display.setEditable(false);
+
+        scrollPane.setViewportView(t_display);
+
+        return panel;
+    }
+
+    private JPanel createInputPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        JTextField textField = new JTextField();
+        sendButton = new JButton("보내기");
+        sendButton.setEnabled(false);
+
+        panel.add(textField, BorderLayout.CENTER);
+        panel.add(sendButton, BorderLayout.EAST);
+
+        // Event Listeners
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = textField.getText();
+                if (text.isEmpty()) {
+                    return;
+                }
+
+                sendMessage(text);
+                printDisplay("나: " + text + "\n");
+                textField.setText("");
+
+                receiveMessage();
+            }
+        };
+
+        textField.addActionListener(listener);
+        sendButton.addActionListener(listener);
+
+        return panel;
+    }
+
+    private JPanel createControlPanel() {
+        JPanel panel = new JPanel(new GridLayout(0, 3));
+
+        JButton connectButton = new JButton("접속하기");
+        JButton disconnectButton = new JButton("접속 끊기");
+        disconnectButton.setEnabled(false);
+        JButton exitButton = new JButton("종료하기");
+
+        panel.add(connectButton);
+        panel.add(disconnectButton);
+        panel.add(exitButton);
+
+        // Event Listeners
+        connectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    connectToServer();
+                } catch (IOException ex) {
+                    System.err.println("클라이언트 접속 오류: " + ex.getMessage());
+                    return;
+                }
+
+                sendButton.setEnabled(true);
+                connectButton.setEnabled(false);
+                disconnectButton.setEnabled(true);
+                exitButton.setEnabled(false);
+            }
+        });
+        disconnectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    disconnect();
+                } catch (IOException ex) {
+                    System.err.println("클라이언트 닫기 오류: " + ex.getMessage());
+                    return;
+                }
+
+                sendButton.setEnabled(false);
+                connectButton.setEnabled(true);
+                disconnectButton.setEnabled(false);
+                exitButton.setEnabled(true);
+            }
+        });
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        return panel;
+    }
+
+    private void printDisplay(String msg) {
+        t_display.append(msg);
+        t_display.setCaretPosition(t_display.getDocument().getLength());
+    }
+
+    /*
+     * socket related methods
+     */
+    private void sendMessage(String msg) {
+        try {
+            out.write(msg + "\n");
+            out.flush();
+        } catch (IOException e) {
+            System.err.println("메시지 전송 오류: " + e.getMessage());
+        }
+    }
+
+    private void receiveMessage() {
+        try {
+            String inMsg = ((BufferedReader) in).readLine();
+            printDisplay("서버: " + inMsg + "\n");
+        } catch (IOException e) {
+            System.err.println("메시지 수신 오류: " + e.getMessage());
+        }
+    }
+
+    private void connectToServer() throws IOException {
+        Socket socket = new Socket(serverAddress, serverPort);
+        OutputStream os = socket.getOutputStream();
+
+        OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+        out = new BufferedWriter(osw);
+
+        InputStreamReader isr = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+        in = new BufferedReader(isr);
+    }
+
+    private void disconnect() throws IOException {
+        out.close();
+    }
+
+    public static void main(String[] args) {
+        String serverAddress = "localhost";
+        int serverPort = 51111;
+
+        new EchoClientGUI(serverAddress, serverPort);
+    }
+}
---
+++ src/project/EchoServerGUI.java
@@ -0,0 +1,154 @@
+package project;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class EchoServerGUI {
+
+    private final JFrame frame;
+    private final int port;
+
+    private JTextArea t_display;
+
+    private ServerSocket serverSocket;
+
+    public EchoServerGUI(int port) {
+        this.port = port;
+
+        frame = new JFrame("EchoServer GUI");
+
+        buildGUI();
+
+        frame.setSize(400, 300);
+        frame.setLocation(900, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    /*
+     * GUI related methods
+     */
+    private void buildGUI() {
+        frame.add(createDisplayPanel(), BorderLayout.CENTER);
+        frame.add(createControlPanel(), BorderLayout.SOUTH);
+    }
+
+    private JPanel createDisplayPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        JScrollPane scrollPane = new JScrollPane();
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        t_display = new JTextArea();
+        t_display.setEditable(false);
+
+        scrollPane.setViewportView(t_display);
+
+        return panel;
+    }
+
+    private JPanel createControlPanel() {
+        JPanel panel = new JPanel(new GridLayout(1, 0));
+
+        JButton exitButton = new JButton("종료");
+        panel.add(exitButton);
+
+        // Event Listeners
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    serverSocket.close();
+                } catch (IOException ex) {
+                    System.err.println("서버 닫기 오류: " + ex.getMessage());
+                    System.exit(-1);
+                }
+                System.exit(0);
+            }
+        });
+
+        return panel;
+    }
+
+    /*
+     * socket related methods
+     */
+    private void startServer() {
+        Socket clientSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            t_display.append("서버가 시작되었습니다.\n");
+
+            while (true) {
+                clientSocket = serverSocket.accept();
+                t_display.append("클라이언트가 연결되었습니다.\n");
+
+                receiveMessages(clientSocket);
+            }
+        } catch (IOException e) {
+            System.err.println("서버 오류: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.err.println("서버 닫기 오류: " + e.getMessage());
+            }
+        }
+    }
+
+    private void receiveMessages(Socket cs) {
+        try {
+            InputStreamReader isr = new InputStreamReader(cs.getInputStream(), StandardCharsets.UTF_8);
+            BufferedReader in = new BufferedReader(isr);
+
+            OutputStreamWriter osw = new OutputStreamWriter(cs.getOutputStream(), StandardCharsets.UTF_8);
+            BufferedWriter out = new BufferedWriter(osw);
+
+            String message;
+            while ((message = in.readLine()) != null) {
+                printDisplay("클라이언트 메시지: " + message + "\n");
+                out.write("'" + message + "' ...echo" + "\n");
+                out.flush();
+            }
+
+            printDisplay("클라이언트가 연결을 종료했습니다.");
+        } catch (IOException e) {
+            System.err.println("서버 읽기 오류: " + e.getMessage());
+        } finally {
+            try {
+                cs.close();
+            } catch (IOException e) {
+                System.err.println("서버 닫기 오류: " + e.getMessage());
+            }
+        }
+    }
+
+    private void printDisplay(String msg) {
+        t_display.append(msg);
+        t_display.setCaretPosition(t_display.getDocument().getLength());
+    }
+
+    public static void main(String[] args) {
+        int port = 51111;
+
+        new EchoServerGUI(port).startServer();
+    }
+}
