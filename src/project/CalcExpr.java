1a52d51ce4ef90bf344b9512d7cbbe2d1410dcf0
enble <enble_777@naver.com>
2024-10-09 17:53:48 +0900

    add CalcGUI & manifest files

---
+++ src/project/CalcExpr.java
@@ -0,0 +1,21 @@
+package project;
+
+import java.io.Serializable;
+
+public class CalcExpr implements Serializable {
+
+    double op1;
+    double op2;
+    char operator;
+
+    public CalcExpr(double op1, char operator, double op2) {
+        this.op1 = op1;
+        this.operator = operator;
+        this.op2 = op2;
+    }
+
+    @Override
+    public String toString() {
+        return op1 + " " + operator + " " + op2;
+    }
+}
